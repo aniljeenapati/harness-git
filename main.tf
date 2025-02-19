7223f50034add82bf9626d6d81b30824c756e15f
aniljeenapati <jeenapati.anil@gmail.com>
2025-02-19 16:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
@@ -10,4 +15,26 @@ resource "google_container_cluster" "primary" {
   }
 
   remove_default_node_pool = false
+}
+
+resource "null_resource" "install_harness_delegate" {
+  depends_on = [google_container_cluster.primary]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud container clusters get-credentials ${var.cluster_name} --region ${var.region} --project ${var.project_id}
+
+      helm repo add harness-delegate https://app.harness.io/storage/harness-download/delegate-helm-chart/
+      helm repo update harness-delegate
+
+      helm upgrade -i helm-delegate --namespace harness-delegate-ng --create-namespace \
+      harness-delegate/harness-delegate-ng \
+      --set delegateName=helm-delegate \
+      --set accountId=ucHySz2jQKKWQweZdXyCog \
+      --set delegateToken=NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY= \
+      --set managerEndpoint=https://app.harness.io \
+      --set delegateDockerImage=harness/delegate:25.01.85000 \
+      --set replicas=1 --set upgrader.enabled=true
+    EOT
+  }
 }

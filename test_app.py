7dd0eae2178a7b9343458689c676bda58b726806
aniljeenapati <jeenapati.anil@gmail.com>
2025-02-24 14:58:56 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from app import app
 
+
 class FlaskAppTestCase(unittest.TestCase):
     
     # Set up the test client
@@ -8,11 +9,13 @@ class FlaskAppTestCase(unittest.TestCase):
         self.client = app.test_client()
         self.client.testing = True
 
+
     # Test the '/' route
     def test_hello(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode('utf-8'), 'Code is Working')
+
 
 if __name__ == '__main__':
     unittest.main()
